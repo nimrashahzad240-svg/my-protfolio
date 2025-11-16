4f3ebc43f69e61443e89ccce001e2609ea2c68a5
nimrashahzad240-svg <nimra.shahzad240@gmail.com>
2025-11-17 00:14:32 +0500

    fixing routing issues

--- newportfolio/src/App.js
+++ newportfolio/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/navbar";
 import Meraki from "./pages/Meraki";
